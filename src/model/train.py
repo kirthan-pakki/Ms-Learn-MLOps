cfc1c9c01c25ef089484545827ec0bd261c665a5
Kirthan Prakash <kirthan.p1398@gmail.com>
2023-08-15 17:05:18 +0000

    Updated the train.py file

--- src/model/train.py
+++ src/model/train.py
@@ -3,17 +3,19 @@
 import argparse
 import glob
 import os
-
+import numpy as np 
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
+import mlflow
+from utils import fetch_logged_data
 
 # define functions
 def main(args):
     # TO DO: enable autologging
 
-
+    mlflow.sklearn.autolog()
     # read data
     df = get_csvs_df(args.training_data)
 
@@ -23,6 +25,13 @@ def main(args):
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
+    run_id = mlflow.last_active_run().info.run_id
+    print("Logged data and model in run {}".format(run_id))
+
+    # show logged data
+    for key, data in fetch_logged_data(run_id).items():
+        print("\n---------- logged {} ----------".format(key))
+        print(data)
 
 def get_csvs_df(path):
     if not os.path.exists(path):
@@ -33,8 +42,16 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    if df == []:
+        raise RuntimeError(f"Cannot use empty dataframe: {df}")
 
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    print("Length of the DataFrame is ", len(X))
+    print("Unique elements in Dependent variable : ", np.unique(y,return_counts=True))
+
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_test=0.3,random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
